4308fe7be1eb85b95aca5776d0f85f3e49bd7162
Kahkasha Naaz <kahkashanaaz2005@gmail.com>
2026-09-23 14:09:04 +0530

    (03)wine_boxplot.py
    Added plt.show() to display the boxplot.

--- Machine Learning-Assignment02/Experiment2(ML)/(03)wine_boxplot.py
+++ Machine Learning-Assignment02/Experiment2(ML)/(03)wine_boxplot.py
@@ -2,14 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.datasets import load_wine
-
-# Load Wine dataset
 wine = load_wine()
-
-# Create DataFrame
 df = pd.DataFrame(wine.data, columns=wine.feature_names)
-
-# Plot boxplots for all numerical attributes
 plt.figure(figsize=(15, 8))
 
 sns.boxplot(data=df)
